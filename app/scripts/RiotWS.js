fc1df30c1a570e99e2968c6ad9447763b96f7a70
Kubeer <kubeek@onet.pl>
2019-03-15 18:22:05 +0100

    Updated RiotWSProtocol

--- app/scripts/RiotWS.js
+++ app/scripts/RiotWS.js
@@ -17,14 +17,18 @@ const MESSAGE_TYPES = {
 class RiotWSProtocol extends WebSocket {
     constructor(url) {
         super(url, 'wamp');
+        this.on('message', this._onMessage.bind(this));
+        this.session = null;
     }
 
     close() {
         super.close();
+        this.session = null;
     }
 
     terminate() {
         super.terminate();
+        this.session = null;
     }
 
     subscribe(event, callback) {
@@ -39,6 +43,29 @@ class RiotWSProtocol extends WebSocket {
 
     send(type, mess) {
         super.send(JSON.stringify([type, mess]));
+    }
+
+    _onMessage(message) {
+        const [type, ...data] = JSON.parse(message);
+
+        switch (type) {
+            case MESSAGE_TYPES.WELCOME:
+                this.session = data[0];
+                break;
+            case MESSAGE_TYPES.CALLRESULT:
+                console.error('Unknown call. Received data: ', data);
+                break;
+            case MESSAGE_TYPES.TYPE_ID_CALLERROR:
+                console.error('Unknown call error. Received data: ', data);
+                break;
+            case MESSAGE_TYPES.EVENT:
+                const [topic, payload] = data;
+                this.emit(topic, payload);
+                break;
+            default:
+                console.error('Unknown error. Received data: ', data);
+                break;
+        }
     }
 }
 
