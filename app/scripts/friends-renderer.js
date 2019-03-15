a9979823d2a3379c197610811c120a210d18437d
Kubeer <kubeek@onet.pl>
2019-03-15 18:20:39 +0100

    Added friend updating

--- app/scripts/friends-renderer.js
+++ app/scripts/friends-renderer.js
@@ -1,9 +1,9 @@
 const electron = require('electron');
 const {ipcRenderer} = electron;
 
-const ul = document.getElementById('friends')
+const ul = document.getElementById('friends');
 
-ipcRenderer.on('addFriend', function(e, friend){
+ipcRenderer.on('addFriend', function(e, friend) {
     var li = document.createElement('li');
     var icon = friend.icon == 37 ? 0 : friend.icon;
     var friendTemplate = `
@@ -13,10 +13,23 @@ ipcRenderer.on('addFriend', function(e, friend){
         <span class="state">${friend.availability}</span>
     </div>`
     li.innerHTML = friendTemplate
+    li.id = friend.id
     li.addEventListener('click', function() {
         openChat(friend)
     })
     ul.appendChild(li)
+})
+
+ipcRenderer.on('updateFriend', function(e, friend) {
+    var li = document.getElementById(friend.id);
+    var icon = friend.lol.profileIcon == 37 ? 0 : friend.lol.profileIcon;
+    var friendTemplate = `
+    <img src="http://raw.communitydragon.org/latest/game/assets/ux/summonericons/profileicon${icon}.png" class="summoner-icon" />
+    <div class="user-data">
+        <span class="username">${friend.name}</span>
+        <span class="state">${friend.availability}</span>
+    </div>`
+    li.innerHTML = friendTemplate
 })
 
 function openChat(friend) {
